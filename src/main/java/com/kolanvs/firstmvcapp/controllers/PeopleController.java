b0b099440a0a039ee6422f39b9b4eb8a1f127bb2
NSBoryaev <NSBoryaev.vniief.ru>
2022-03-22 16:13:47 +0300

    Add Dao (Lesson 21) stop at 28:44

---
+++ src/main/java/com/kolanvs/firstmvcapp/controllers/PeopleController.java
@@ -0,0 +1,38 @@
+package com.kolanvs.firstmvcapp.controllers;
+
+import com.kolanvs.firstmvcapp.dao.PersonDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PeopleController(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        //Получим всех людей из DAO и передадим в представление
+        model.addAttribute("people", personDao.index());
+        return "people";
+    }
+
+    @GetMapping("/{id}")
+    public String show(
+            @PathVariable("id") int id,
+            Model model) {
+        //Получим одного человека из DAO
+        model.addAttribute("person", personDao.show(id));
+        return "people/show";
+    }
+}
---
+++ src/main/java/com/kolanvs/firstmvcapp/dao/PersonDao.java
@@ -0,0 +1,32 @@
+package com.kolanvs.firstmvcapp.dao;
+
+import com.kolanvs.firstmvcapp.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDao {
+    private static int PEOPLE_COUNT = 0;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+}
