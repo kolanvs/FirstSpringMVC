7c21526d96ee4043790d7d134c4f8b11f64ee44b
Nikkolas-user <kolanvs@yandex.ru>
2022-03-23 21:16:54 +0300

    Add JdbcTemplate (Lesson 26) - END VIDEO COURSES

--- src/main/java/com/kolanvs/firstmvcapp/dao/PersonDAO.java
+++ src/main/java/com/kolanvs/firstmvcapp/dao/PersonDAO.java
@@ -1,5 +1,8 @@
 package com.kolanvs.firstmvcapp.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import com.kolanvs.firstmvcapp.models.Person;
 
@@ -12,88 +15,43 @@ import java.util.List;
  */
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-    private List<Person> people;
 
-    private static final String URL =
-            "jdbc:postgresql://172.16.179.67:5432/epamtutor";
+    public final JdbcTemplate jdbcTemplate;
 
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "12345";
-
-    private static Connection connection;
-
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        }
-        catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Person> index() {
-        List<Person> people = new ArrayList<>();
 
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "select * from Person";
-            ResultSet resultSet = statement.executeQuery(SQL);
-
-            while(resultSet.next()) {
-                Person person = new Person();
-
-                person.setId(resultSet.getInt("id"));
-                person.setName(resultSet.getString("name"));
-                person.setAge(resultSet.getInt("age"));
-                person.setEmail(resultSet.getString("email"));
-
-                people.add(person);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return people;
+        return jdbcTemplate.query("select * from Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id) {
-        //return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-        return null;
+        return jdbcTemplate.query("select * from Person where id=?",
+                new Object[]{id},
+                new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
-//        person.setId(++PEOPLE_COUNT);
-//        people.add(person);
 
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "insert into Person values(" + ++PEOPLE_COUNT + ", '" + person.getName() +
-                    "', '" + person.getAge() + "', '" + person.getEmail() + "');";
-            statement.executeUpdate(SQL);
+        jdbcTemplate.update("insert into Person values (1, ?, ?, ?)", person.getName(), person.getAge(),
+                person.getEmail());
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
+        jdbcTemplate.update("update person set name = ?, age = ?, email = ? where id = ?",
+                updatedPerson.getName(),
+                updatedPerson.getAge(),
+                updatedPerson.getEmail(),
+                id);
 
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+        jdbcTemplate.update("delete from person where id = ?", id);
     }
 }
