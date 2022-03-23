0fe59d7c294a79092188a578ee0dfc34fd354036
NSBoryaev <NSBoryaev.vniief.ru>
2022-03-23 10:28:47 +0300

    Add Post (Lesson 22)

--- src/main/java/com/kolanvs/firstmvcapp/controllers/PeopleController.java
+++ src/main/java/com/kolanvs/firstmvcapp/controllers/PeopleController.java
@@ -1,13 +1,11 @@
 package com.kolanvs.firstmvcapp.controllers;
 
 import com.kolanvs.firstmvcapp.dao.PersonDao;
+import com.kolanvs.firstmvcapp.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -34,5 +32,16 @@ public class PeopleController {
         //Получим одного человека из DAO
         model.addAttribute("person", personDao.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDao.save(person);
+        return "redirect:/people";
     }
 }
--- src/main/java/com/kolanvs/firstmvcapp/dao/PersonDao.java
+++ src/main/java/com/kolanvs/firstmvcapp/dao/PersonDao.java
@@ -29,4 +29,9 @@ public class PersonDao {
                 .findAny()
                 .orElse(null);
     }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
 }
